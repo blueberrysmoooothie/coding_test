2b5a0f3ff9f7d8849f7d996be59523b399bc2f7f
loading <93635517+blueberrysmoooothie@users.noreply.github.com>
2023-06-02 11:02:09 +0900

    06/02

---
+++ programers/level0/pro181928.java
@@ -0,0 +1,26 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/181928
+
+public class pro181928 {
+    public int solution(int[] num_list) {
+        String odd_nums = "";
+        String even_nums = "";
+        for (int index = 0; index < num_list.length; index++) {
+            int num = num_list[index];
+            if (num % 2 == 1) {
+                odd_nums += (char) num;
+            } else {
+                even_nums += (char) num;
+            }
+        }
+
+        int answer = Integer.valueOf(odd_nums) + Integer.valueOf(even_nums);
+
+        return answer;
+    }
+
+    public void main() {
+        int[] num_list = { 1, 2, 3 };
+        int answer = solution(num_list);
+        System.out.println(answer);
+    }
+}
---
+++ programers/level0/pro181949.java
@@ -0,0 +1,24 @@
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181949
+import java.util.Scanner;
+
+public class pro181949 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String new_a = "";
+        for (int index = 0; index < a.length(); index++) {
+            new_a += changeChar(a.charAt(index));
+        }
+        System.out.println(new_a);
+
+    }
+
+    private static char changeChar(char alp) {
+        if (Character.isUpperCase(alp)) {
+            return Character.toLowerCase(alp);
+        } else {
+            return Character.toUpperCase(alp);
+        }
+    }
+}
